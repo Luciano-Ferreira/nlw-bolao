0be347bbe79e35adfc16e23560d7209bb0ddcd30
lfs <luciano2023silva@gmail.com>
2022-11-01 16:37:54 -0300

    config: expo in the monorepo

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ```bash
 
 ```
+
+Diagrama ERD
--- packages/mobile/__tests__/mobile.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const mobile = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(mobile(), 'Hello from mobile');
-console.info("mobile tests passed");
