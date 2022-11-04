4f8b93e70bf274074cd555d702dae9b94b1cc25b
lfs <luciano2023silva@gmail.com>
2022-11-04 16:15:16 -0300

    feat: list games and if i'm there route

--- packages/server/src/routes/game.ts
+++ packages/server/src/routes/game.ts
@@ -1,9 +1,46 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
 import { prisma } from '../lib/prisma';
+import { authenticate } from '../plugins/authenticate';
 
 export async function gameRoutes(fastify: FastifyInstance) {
-   fastify.get("/games/count", async () => {
-    const count = await prisma.game.count();
-    return { count };
+   fastify.get("/pools/:id/game",
+    {
+      onRequest: [authenticate]
+    },
+    async (req) => {
+      const getPoolsParams = z.object({
+        id: z.string(),
+  
+      });
+      const { id } = getPoolsParams.parse(req.params)
+
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: 'desc',
+        },
+        include: {
+          guesses: {
+            where: {
+              participant: {
+                userId: req.user.sub,
+                poolId: id
+              }
+            }
+          }
+        }
+      });
+
+      return {
+        // alterando o retorno para que retorne apenas o id do meu palpite ao inves de um array com apenas um palpite.
+        // e removendo esse array de guesses
+        games: games.map(game => {
+          return {
+            ...game,
+            guess: game.guesses.length > 0 ? game.guesses[0] : null,
+            guesses: undefined 
+          }
+        })
+      }
   });
 }
