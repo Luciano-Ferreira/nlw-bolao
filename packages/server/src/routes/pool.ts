1fd228952f02ad1ad0c000638683a9c83120044a
lfs <luciano2023silva@gmail.com>
2022-11-04 02:48:22 -0300

    feat: error handling if use already joined or pool will not find

--- packages/server/src/routes/pool.ts
+++ packages/server/src/routes/pool.ts
@@ -1,8 +1,9 @@
-import { FastifyInstance, FastifyRequest } from "fastify";
+import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 
 import { z } from "zod";
 import ShortUniqueId from "short-unique-id";
+import { authenticate } from "../plugins/authenticate";
 
 export async function poolRoutes(fastify: FastifyInstance) {
   fastify.get("/pools/count", async () => {
@@ -10,7 +11,7 @@ export async function poolRoutes(fastify: FastifyInstance) {
     return { count };
   });
 
-  fastify.post("/pools", async (req, res) => {
+  fastify.post("/pools", async (req) => {
     const createPoolBody = z.object({
       title: z.string(),
     });
@@ -21,30 +22,76 @@ export async function poolRoutes(fastify: FastifyInstance) {
     const code = String(generate()).toUpperCase();
 
     try {
-      await req.jwtVerify()
+      await req.jwtVerify();
 
       await prisma.pool.create({
         data: {
           title,
           code,
           ownerId: req.user.sub,
-          
+
           participants: {
             create: {
-              userId: req.user.sub
-            }
-          }
+              userId: req.user.sub,
+            },
+          },
         },
       });
     } catch (err) {
       await prisma.pool.create({
         data: {
           title,
-          code
+          code,
         },
       });
     }
-
-    
   });
+
+  fastify.post(
+    "pools/:id/",
+    {
+      onRequest: [authenticate],
+    },
+    async (req, rep) => {
+      const joinPoolBody = z.object({
+        code: z.string(),
+      });
+
+      const { code } = joinPoolBody.parse(req.body);
+
+      const pool = await prisma.pool.findUnique({
+        where: {
+          code,
+        },
+        include: {
+          participants: {
+            where: {
+              userId: req.user.sub,
+            },
+          },
+        },
+      });
+
+      if (!pool) {
+        return rep.status(400).send({
+          message: "Pool not found!",
+        });
+      }
+
+      if (pool.participants.length > 0) {
+        return rep.status(400).send({
+          message: 'You already joined this pool!'
+        })
+      }
+
+      await prisma.participant.create({
+        data: {
+          poolId: pool.id,
+          userId: req.user.sub
+        }
+      });
+
+      return rep.status(201).send()
+    }
+  );
 }
