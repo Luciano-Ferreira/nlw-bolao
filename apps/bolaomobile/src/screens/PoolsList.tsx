ab78e99bf4ebd31aca956b23a7a58408f552487e
lfs <luciano2023silva@gmail.com>
2022-11-05 14:56:40 -0300

    feat: list pools with useFocusEffect

--- apps/bolaomobile/src/screens/PoolsList.tsx
+++ apps/bolaomobile/src/screens/PoolsList.tsx
@@ -1,27 +1,80 @@
-import { VStack, Icon } from 'native-base';
+import { useCallback, useEffect, useState } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
-import { Octicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import { Button } from '../components/Button';
-import { Header } from '../components/Header';
+import { Octicons } from "@expo/vector-icons";
+
+import { Button } from "../components/Button";
+import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
+import { PoolCard, PoolCardProps } from "../components/PoolCard";
+import { EmptyPoolList } from "../components/EmptyPoolList";
+
+import { api } from "../services/api";
 
 export function PoolsList() {
-  const navigation = useNavigation();
+  const [isLoading, setISLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardProps[]>([]);
 
-  const whatsRoute = () => navigation.navigate('findPool');
+  const { navigate } = useNavigation();
+  const toast = useToast();
 
-  const lupe = <Icon as={Octicons} name='search' color='black' size='md' />
+  async function fetchPools() {
+    try {
+      setISLoading(true);
+      const response = await api.get("/pools");
+
+      setPools(response.data.pools);
+    } catch (err) {
+      console.log(err);
+      toast.show({
+        title: "Não foi possivel carregar os bolões",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setISLoading(false);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchPools();
+  }, []));
+
+  const whatsRoute = () => navigate("findPool");
+
+  const lupe = <Icon as={Octicons} name="search" color="black" size="md" />;
 
   return (
-    <VStack flex={1} bgColor='gray.900'>
-      <Header title='Meus bolões' />
-      <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor='gray.600' pb={4} mb={4}>
-        <Button 
-          title='Buscar Bolão por código'
+    <VStack flex={1} bgColor="gray.900">
+      <Header title="Meus bolões" />
+      <VStack
+        mt={6}
+        mx={5}
+        borderBottomWidth={1}
+        borderBottomColor="gray.600"
+        pb={4}
+        mb={4}
+      >
+        <Button
+          title="Buscar Bolão por código"
           leftIcon={lupe}
           onPress={whatsRoute}
         />
       </VStack>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={pools}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          px={5}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          ListEmptyComponent={() => <EmptyPoolList />}
+        />
+      )}
     </VStack>
-  )
+  );
 }
