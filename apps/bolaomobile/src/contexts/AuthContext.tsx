d237e56c8823b6ee054540a6df34d142be56ad5a
lfs <luciano2023silva@gmail.com>
2022-11-05 01:36:37 -0300

    feat: authentication restAPI

--- apps/bolaomobile/src/contexts/AuthContext.tsx
+++ apps/bolaomobile/src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from 'react';
 import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
+import { api } from '../services/api';
 
 WebBrowser.maybeCompleteAuthSession()
 export interface UserProps {
@@ -53,7 +54,24 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   async function signInWithGoogle(access_token: string) {
-    console.log('Token de autenticação: ' + access_token)
+    try {
+      setIsUserLoading(true);
+
+      const tokenResponse = await api.post('/users', {
+        access_token
+      });
+      api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`;
+
+      const userInfoResponse = await api.get('/me');
+
+      setUser(userInfoResponse.data.user)
+
+    } catch (err) {
+      console.log(err)
+      throw err
+    } finally {
+      setIsUserLoading(false)
+    }
   }
 
   useEffect(() => {
